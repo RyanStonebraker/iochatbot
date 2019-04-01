df3789562203f8f1019e23e310a8f13e2fe423dd
Ryan Stonebraker <ryanastonebraker@gmail.com>
2019-04-01 13:26:19 -0800

    Adds user entry

--- main.py
+++ main.py
@@ -4,7 +4,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("main.html")
+    agentNames = [
+        "angry",
+        "sad",
+        "happy",
+        "disgust"
+    ]
+
+    agents = [
+        {
+            "name": "angry",
+            "response": "testing angry response"
+        },
+        {
+            "name": "sad",
+            "response": "testing sad response"
+        },
+        {
+            "name": "happy",
+            "response": "testing happy response"
+        },
+        {
+            "name": "disgust",
+            "response": "testing disgust response"
+        }
+    ]
+    return render_template("main.html", agents=agents)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
