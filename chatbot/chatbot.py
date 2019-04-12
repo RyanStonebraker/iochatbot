9166ad48317b46066b56205de455179f55bef910
Ryan Stonebraker <ryanastonebraker@gmail.com>
2019-04-11 16:36:13 -0800

    Adds random poetry to individual template sets

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -27,16 +27,16 @@ class Environment:
         self.db = database
 
         self.templatingEngines["sad"] = templateEngine.TemplateEngine(corpus="sadFriendsTemplates.txt", commonWords="sadWords.txt", emotion="sad")
-        self.agents["sad"] = agent.Agent(corpus="generated/sad.txt", verbose=False, cacheCorpus=False)
+        self.agents["sad"] = agent.Agent(corpus="generated/sad.txt", verbose=False, cacheCorpus=False, defaultMessage="Maybe. What about you?")
 
         self.templatingEngines["angry"] = templateEngine.TemplateEngine(corpus="angryFriendsTemplates.txt", commonWords="angryWords.txt", emotion="angry")
-        self.agents["angry"] = agent.Agent(corpus="generated/angry.txt", verbose=False, cacheCorpus=False)
+        self.agents["angry"] = agent.Agent(corpus="generated/angry.txt", verbose=False, cacheCorpus=False, defaultMessage="Are you?")
 
         self.templatingEngines["happy"] = templateEngine.TemplateEngine(corpus="happyFriendsTemplates.txt", commonWords="happyWords.txt", emotion="happy")
-        self.agents["happy"] = agent.Agent(corpus="generated/happy.txt", verbose=False, cacheCorpus=False)
+        self.agents["happy"] = agent.Agent(corpus="generated/happy.txt", verbose=False, cacheCorpus=False, defaultMessage="What about you?")
 
         self.templatingEngines["disgust"] = templateEngine.TemplateEngine(corpus="disgustFriendsTemplates.txt", commonWords="disgust_words.txt", emotion="disgust")
-        self.agents["disgust"] = agent.Agent(corpus="generated/disgust.txt", verbose=False, cacheCorpus=False)
+        self.agents["disgust"] = agent.Agent(corpus="generated/disgust.txt", verbose=False, cacheCorpus=False, defaultMessage="Definitely not. Are you?")
 
         self.conversationUUID = str(uuid.uuid4())
 
